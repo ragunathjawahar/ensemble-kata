6bfef2d415c9773c659d749a4476e26be65e3335
sigirisaicharan <saicharansigiri@gmail.com>
2023-09-30 07:22:31 +0530

    rename variables

--- src/main/kotlin/Stanza.kt
+++ src/main/kotlin/Stanza.kt
@@ -11,20 +11,20 @@ class Stanza {
       }
 
       1 -> {
-        val s = """1 bottle"""
-        val s1 = """no more bottles"""
+        val initialBeerCountStr = """1 bottle"""
+        val leftOutBeerCountStr = """no more bottles"""
         """
-        $s of beer on the wall, 1 bottle of beer.
-        Take one down and pass it around, $s1 of beer on the wall.
+        $initialBeerCountStr of beer on the wall, 1 bottle of beer.
+        Take one down and pass it around, $leftOutBeerCountStr of beer on the wall.
       """
       }
 
       else -> {
-        val s = """$beerCount bottles"""
-        val s1 = """${beerCount - 1} bottles"""
+        val initialBeerCountStr = """$beerCount bottles"""
+        val leftOutBeerCountStr = """${beerCount - 1} bottles"""
         """
-      $s of beer on the wall, $beerCount bottles of beer.
-      Take one down and pass it around, $s1 of beer on the wall.
+      $initialBeerCountStr of beer on the wall, $beerCount bottles of beer.
+      Take one down and pass it around, $leftOutBeerCountStr of beer on the wall.
     """
       }
     }.trimIndent()
